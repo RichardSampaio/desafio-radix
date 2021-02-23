dce95c49ac760fe67a97d426cafeea1b899de617
Richard Sampaio <richardsampaio90@gmail.com>
2021-02-23 17:41:25 -0300

    faltando coisas ainda

---
+++ EventosPeloBrasil/EventosPeloBrasil/Controllers/EventosApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EventosPeloBrasil.Models;
+
+namespace EventosPeloBrasil.Controllers
+{
+    public class EventosApiController : ApiController
+    {
+        private eventospelobrasilEntities db = new eventospelobrasilEntities();
+
+        // GET: api/EventosApi
+        public IQueryable<eventos> Geteventos()
+        {
+            return db.eventos;
+        }
+
+        // GET: api/EventosApi/5
+        [ResponseType(typeof(eventos))]
+        public IHttpActionResult Geteventos(int id)
+        {
+            eventos eventos = db.eventos.Find(id);
+            if (eventos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(eventos);
+        }
+
+        // PUT: api/EventosApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puteventos(int id, eventos eventos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != eventos.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(eventos).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!eventosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/EventosApi
+        [ResponseType(typeof(eventos))]
+        public IHttpActionResult Posteventos(eventos eventos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.eventos.Add(eventos);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = eventos.Id }, eventos);
+        }
+
+        // DELETE: api/EventosApi/5
+        [ResponseType(typeof(eventos))]
+        public IHttpActionResult Deleteeventos(int id)
+        {
+            eventos eventos = db.eventos.Find(id);
+            if (eventos == null)
+            {
+                return NotFound();
+            }
+
+            db.eventos.Remove(eventos);
+            db.SaveChanges();
+
+            return Ok(eventos);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool eventosExists(int id)
+        {
+            return db.eventos.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ EventosPeloBrasil/EventosPeloBrasil/Controllers/EventosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EventosPeloBrasil.Models;
+
+namespace EventosPeloBrasil.Controllers
+{
+    public class EventosController : Controller
+    {
+        private eventospelobrasilEntities db = new eventospelobrasilEntities();
+
+        // GET: Eventos
+        public ActionResult Index()
+        {
+            return View(db.eventos.ToList());
+        }
+
+        // GET: Eventos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eventos eventos = db.eventos.Find(id);
+            if (eventos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventos);
+        }
+
+        // GET: Eventos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Eventos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,DataEvento,Tag,Valor,Estado")] eventos eventos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.eventos.Add(eventos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eventos);
+        }
+
+        // GET: Eventos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eventos eventos = db.eventos.Find(id);
+            if (eventos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventos);
+        }
+
+        // POST: Eventos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,DataEvento,Tag,Valor,Estado")] eventos eventos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eventos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eventos);
+        }
+
+        // GET: Eventos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eventos eventos = db.eventos.Find(id);
+            if (eventos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventos);
+        }
+
+        // POST: Eventos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            eventos eventos = db.eventos.Find(id);
+            db.eventos.Remove(eventos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
